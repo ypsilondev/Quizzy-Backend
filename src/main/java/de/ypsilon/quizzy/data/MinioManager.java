57c9f7ee7f5ddabbbc8aad81c963c26797718d76
Julian <yJulian@outlook.de>
2021-04-08 17:36:48 +0200

    Add minio to dependencies. Wired the DatabaseManager and MinioManager. Introduced a template.env file

--- src/main/java/de/ypsilon/quizzy/data/MinioManager.java
+++ src/main/java/de/ypsilon/quizzy/data/MinioManager.java
@@ -1,8 +1,36 @@
 package de.ypsilon.quizzy.data;
 
+import io.minio.MinioClient;
+
+/**
+ * Class to handle all transactions to the minio database.
+ *
+ * @version 1.0
+ */
 public class MinioManager {
 
+    private final MinioClient client;
+
+    /**
+     * Build the minio manager and initialize the connection to the minio database.
+     */
     public MinioManager() {
-        //TODO Initialize Minio
+        String minioSecretKey = System.getenv("minio.key.secret");
+        String minioAccessKey = System.getenv("minio.key.access");
+        String minioEndpoint = System.getenv("minio.endpoint");
+
+        this.client = MinioClient.builder()
+                        .endpoint(minioEndpoint)
+                        .credentials(minioAccessKey, minioSecretKey)
+                        .build();
+    }
+
+    /**
+     * Get the minio client.
+     *
+     * @return the registered minio client.
+     */
+    public MinioClient getClient() {
+        return client;
     }
 }
